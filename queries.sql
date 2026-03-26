db29cddcbb80a883d57c94732c1b4b767227794b
paulgnuskov-art <paulgnuskov@gmail.com>
2026-03-26 14:00:57 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -11,4 +11,27 @@ JOIN products p ON p.product_id = s.product_id
 GROUP BY e.employee_id, e.first_name, e.last_name
 ORDER BY income DESC, seller
 LIMIT 10;
---Шаг № 5 
+--Шаг № 5 отчет с продавцами, чья выручка ниже средней выручки всех продавцов.
+WITH seller_avg AS (
+    SELECT
+        e.employee_id,
+        TRIM(CONCAT(e.first_name, ' ', e.last_name)) AS seller,
+        AVG(p.price * s.quantity) AS avg_income
+    FROM sales s
+    JOIN employees e ON e.employee_id = s.sales_person_id
+    JOIN products p ON p.product_id = s.product_id
+    GROUP BY e.employee_id, e.first_name, e.last_name
+),
+global_avg AS (
+    SELECT AVG(p.price * s.quantity) AS avg_income
+    FROM sales s
+    JOIN products p ON p.product_id = s.product_id
+)
+SELECT
+    sa.seller,
+    FLOOR(sa.avg_income)::int AS average_income
+FROM seller_avg sa
+CROSS JOIN global_avg ga
+WHERE sa.avg_income < ga.avg_income
+ORDER BY average_income ASC, seller;
+--Шаг № 5 отчет с данными по выручке по каждому продавцу и дню недели.
