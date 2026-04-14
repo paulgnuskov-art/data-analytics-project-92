ecfa282450c200b4b50a22b2bdc3e2010fcd6cfe
paulgnuskov-art <paulgnuskov@gmail.com>
2026-04-14 15:33:12 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -56,57 +56,30 @@ ORDER BY
     average_income ASC,
     sa.seller ASC;
 -- Шаг 5. Отчет с данными по выручке по каждому продавцу и дню недели.
-WITH weekday_sales AS (
-    SELECT
-        s.sale_date,
-        p.price,
-        s.quantity,
-        TRIM(
-            CONCAT(e.first_name, ' ', e.last_name)
-        ) AS seller
-    FROM sales AS s
-    INNER JOIN employees AS e
-        ON s.sales_person_id = e.employee_id
-    INNER JOIN products AS p
-        ON s.product_id = p.product_id
-),
-
-seller_weekday_income AS (
-    SELECT
-        ws.seller,
-        EXTRACT(ISODOW FROM ws.sale_date)::int AS day_num,
-        FLOOR(SUM(ws.price * ws.quantity))::bigint AS income
-    FROM weekday_sales AS ws
-    GROUP BY
-        ws.seller,
-        EXTRACT(ISODOW FROM ws.sale_date)
-),
-
-weekday_report AS (
-    SELECT
-        swi.seller,
-        swi.day_num,
-        swi.income,
-        CASE swi.day_num
-            WHEN 1 THEN 'monday'
-            WHEN 2 THEN 'tuesday'
-            WHEN 3 THEN 'wednesday'
-            WHEN 4 THEN 'thursday'
-            WHEN 5 THEN 'friday'
-            WHEN 6 THEN 'saturday'
-            WHEN 7 THEN 'sunday'
-        END AS day_of_week
-    FROM seller_weekday_income AS swi
-)
-
 SELECT
-    wr.seller,
-    wr.day_of_week,
-    wr.income
-FROM weekday_report AS wr
+    TRIM(CONCAT(e.first_name, ' ', e.last_name)) AS seller,
+    CASE EXTRACT(ISODOW FROM s.sale_date)
+        WHEN 1 THEN 'monday'
+        WHEN 2 THEN 'tuesday'
+        WHEN 3 THEN 'wednesday'
+        WHEN 4 THEN 'thursday'
+        WHEN 5 THEN 'friday'
+        WHEN 6 THEN 'saturday'
+        WHEN 7 THEN 'sunday'
+    END AS day_of_week,
+    FLOOR(SUM(p.price * s.quantity))::bigint AS income
+FROM sales AS s
+INNER JOIN employees AS e
+    ON s.sales_person_id = e.employee_id
+INNER JOIN products AS p
+    ON s.product_id = p.product_id
+GROUP BY
+    e.employee_id,
+    seller,
+    EXTRACT(ISODOW FROM s.sale_date)
 ORDER BY
-    wr.day_num ASC,
-    wr.seller ASC;
+    EXTRACT(ISODOW FROM s.sale_date) ASC,
+    seller ASC;
 -- Шаг 6. Количество покупателей в разных возрастных группах.
 WITH age_groups_base AS (
     SELECT
