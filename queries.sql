faf06ab127eb12399a58bea15f1a3d4a9f64f55c
paulgnuskov-art <paulgnuskov@gmail.com>
2026-04-15 21:57:13 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -146,12 +146,12 @@ GROUP BY
 ORDER BY
     agb.age_category ASC;
 
--- Шаг 6. Данные по количеству уникальных покупателей и выручке, которую они принесли.
-with monthly_sales AS (
+-- Шаг 6. Данные по количеству уникальных покупателей и выручке.
+WITH monthly_sales AS (
     SELECT
         TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month,
-        COUNT(DISTINCT s.customer_id) AS total_customers,
         FLOOR(SUM(p.price * s.quantity))::bigint AS income
+        COUNT(DISTINCT s.customer_id) AS total_customers
     FROM sales AS s
     INNER JOIN products AS p
         ON s.product_id = p.product_id
